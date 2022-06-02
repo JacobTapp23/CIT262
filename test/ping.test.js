10e4553d3889569325f9c3458dfa1466aabdafec
JacobTapp23 <jacobtapp12@byui.edu>
2022-06-02 09:54:34 -0600

    fixed ping formatting

--- test/ping.test.js
+++ test/ping.test.js
@@ -1,7 +1,7 @@
 import  fetch  from 'node-fetch';
 
 
-it ('Should respond pong', async () =>{
+it ('Should respond', async () =>{
     const answer = await fetch('http://34.145.101.133/api/ping').then(res => res.json())
     console.log(answer);
     
